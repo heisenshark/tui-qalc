edaf3116d9ed8c809d09bb4b9974bef97e17df19
Sylay <sylay@pop-os.localdomain>
2024-03-22 00:29:27 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use core::panic;
-use std::borrow::Borrow;
-use std::io::Read;
-use std::os::unix::process::CommandExt;
 use std::process::{Child, Command};
-
-use crossterm::event::KeyCode;
 use cursive::event::Key;
-use cursive::theme::{PaletteStyle, Style, StyleType};
-use cursive::view::ViewWrapper;
+use cursive::theme::{Style};
 use cursive::views::{
     Button, Dialog, DummyView, EditView, LinearLayout, NamedView, SelectView, TextView,
 };
@@ -24,27 +17,9 @@ fn main() {
     println!("{:?}", String::from_utf8(x));
     let history: Vec<String> = vec![];
 
-    // return;
-    // let select = SelectView::<String>::new()
-    //     .on_submit(on_submit)
-    //     .with_name("select")
-    //     .fixed_size((10, 5));
-    //
-    // let buttons = LinearLayout::vertical()
-    //     .child(Button::new("Add new", add_name))
-    //     .child(Button::new("Delete", delete_name))
-    //     .child(DummyView)
-    //     .child(Button::new("Quit", Cursive::quit))
-    //
-    // siv.add_layer(Dialog::around(LinearLayout::horizontal()
-    //         .child(select)
-    //         .child(DummyView)
-    //         .child(buttons))
-    //     .title("Select a profile"));
-    // panic!("dupa");
-    // "d.".sub== "error";
     siv.add_global_callback(Key::Enter, |s| {
         let e = s.find_name::<EditView>("editv").unwrap();
+
         s.call_on_name("history", |history_view: &mut SelectView<String>| {
             history_view.add_item(e.get_content().to_string(), e.get_content().to_string());
         });
@@ -83,53 +58,4 @@ fn main() {
         .full_screen();
     siv.add_layer(layout);
     siv.run();
-}
-
-fn add_name(s: &mut Cursive) {
-    fn ok(s: &mut Cursive, name: &str) {
-        s.call_on_name("select", |view: &mut SelectView<String>| {
-            view.add_item_str(name)
-        });
-        s.pop_layer();
-    }
-
-    s.add_layer(
-        Dialog::around(
-            EditView::new()
-                .on_submit(ok)
-                .with_name("name")
-                .fixed_width(10),
-        )
-        .title("Enter a new name")
-        .button("Ok", |s| {
-            let name = s
-                .call_on_name("name", |view: &mut EditView| view.get_content())
-                .unwrap();
-            ok(s, &name);
-        })
-        .button("Cancel", |s| {
-            s.pop_layer();
-        }),
-    );
-}
-
-fn delete_name(s: &mut Cursive) {
-    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
-    match select.selected_id() {
-        None => s.add_layer(Dialog::info("No name to remove")),
-        Some(focus) => {
-            select.remove_item(focus);
-        }
-    }
-}
-
-fn on_submit(s: &mut Cursive, name: &str) {
-    // s.pop_layer();
-    s.add_layer(
-        Dialog::text(format!("Name: {}\nAwesome: yes", name))
-            .title(format!("{}'s info", name))
-            .button("Quit", |s| {
-                s.pop_layer();
-            }),
-    );
 }
